372ed5a09ca1fe316206ec7dc1463f733970ce4f
Abdurrahman Wafi <54539599+KasiiihWafi@users.noreply.github.com>
2019-12-02 20:35:57 +0700

    Initial commit

---
+++ app/src/main/java/coding/stopwatch/com/stopwatch/SplashActivity.java
@@ -0,0 +1,25 @@
+package coding.stopwatch.com.stopwatch;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    int niali = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        }, niali);
+
+    }
+}
